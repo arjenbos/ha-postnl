46c4b1db0beae8135f3d635bcf920f54a331ef53
Arjen Bos <1064589+arjenbos@users.noreply.github.com>
2024-01-18 13:57:22 +0100

    Refresh after 90 instead of 120 seconds

--- custom_components/postnl/coordinator.py
+++ custom_components/postnl/coordinator.py
@@ -24,7 +24,7 @@ class PostNLCoordinator(DataUpdateCoordinator):
             hass,
             _LOGGER,
             name="PostNL",
-            update_interval=timedelta(seconds=20),
+            update_interval=timedelta(seconds=90),
         )
 
     async def _async_update_data(self) -> dict[str, tuple[Package]]:
