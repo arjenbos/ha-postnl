5bbf60c26aa7f91dde96d41131318eae3ef42e0a
geert36 <77209691+geert36@users.noreply.github.com>
2024-12-27 16:39:24 +0100

    Update package.py

--- custom_components/postnl/structs/package.py
+++ custom_components/postnl/structs/package.py
@@ -2,9 +2,11 @@ class Package:
     key: str
     name: str
     url: str
+    shipment_type: str
     status_message: str
     delivered: bool
     delivery_date: str | None
+    delivery_address_type: str | None
     planned_date: str | None
     planned_from: str | None
     planned_to: str | None
@@ -15,9 +17,11 @@ class Package:
             key: str,
             name: str,
             url: str,
+            shipment_type: str,
             status_message: str,
             delivered: bool,
             delivery_date: str | None = None,
+            delivery_address_type: str | None = None,
             planned_date: str | None = None,
             planned_from: str | None = None,
             planned_to: str | None = None,
@@ -26,13 +30,12 @@ class Package:
         self.key = key
         self.name = name
         self.url = url
+        self.shipment_type = shipment_type
         self.status_message = status_message
         self.delivered = delivered
         self.delivery_date = delivery_date
+        self.delivery_address_type = delivery_address_type
         self.planned_date = planned_date
         self.planned_from = planned_from
         self.planned_to = planned_to
         self.expected_datetime = expected_datetime
-
-
-
